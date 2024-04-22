245958a700ff7ab46ceeaafdfcbc6226d950a020
Magnus <amarcellin@wedigital.garden>
2024-04-22 17:11:09 +0200

    :sparkles: add new options

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import {ActionMessage} from "@workadventure/iframe-api-typings";
 
 console.log('Script started successfully');
+
+
+function displayDoor(state: boolean) { 
+    if (state === true) {
+        WA.room.showLayer('door/door_opened');
+        WA.room.hideLayer('door/door_closed');
+    } else {
+        WA.room.hideLayer('door/door_opened');
+        WA.room.showLayer('door/door_closed');
+    }
+}
+
 
 let currentPopup: any = undefined;
 
@@ -23,6 +36,40 @@ WA.onInit().then(() => {
         const time = today.getHours() + ":" + today.getMinutes();
         currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
     })
+
+    WA.room.area.onEnter('bowling').subscribe(() => {
+        currentPopup = WA.ui.openPopup("bowling", "C'est du bowling", []);
+    })
+
+    let noteWebsite: any;
+
+    WA.room.onEnterLayer("visibleNote").subscribe(async () => {
+        console.log("Entering visibleNote layer");
+        noteWebsite = await WA.ui.website.open({
+            url: "./src/note.html",
+            position: {
+                vertical: "top",
+                horizontal: "middle",
+            },
+            size: {
+                height: "30vh",
+                width: "50vw",
+            },
+            margin: {
+                top: "10vh",
+            },
+            allowApi: true,
+        });
+
+    });
+
+    WA.room.onLeaveLayer("visibleNote").subscribe(() => {
+        noteWebsite.close();
+    });
+
+
+    WA.room.area.onLeave('bowling').subscribe(closePopup)
+
 
     WA.room.area.onLeave('clock').subscribe(closePopup)
 
