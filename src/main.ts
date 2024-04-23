73880107b1fc19930e32f6ccb498b20f41a05f14
Magnus <amarcellin@wedigital.garden>
2024-04-23 15:48:11 +0200

    working door

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import {ActionMessage} from "@workadventure/iframe-api-typings";
 
 console.log('Script started successfully');
 
+function displayDoor(state: boolean) { 
+    if (state === true) {
+        WA.room.showLayer('door/door_opened');
+        WA.room.hideLayer('door/door_closed');
+    } else {
+        WA.room.hideLayer('door/door_opened');
+        WA.room.showLayer('door/door_closed');
+    }
+}
 
 let currentPopup: any = undefined;
 
@@ -12,19 +22,44 @@ WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
+    displayDoor(WA.state.doorState as boolean);
+
     let player = (Math.random()>=0.5)? 1 : 0;
-    console.log('est visible : ' + player)
 
     if (player === 1) {
         WA.room.hideLayer("above/laptops");
     }
+
+
+    WA.state.onVariableChange('doorState').subscribe((doorState) => {
+        displayDoor(doorState as boolean);
+    });
+
+    let openCloseMessage: ActionMessage | undefined;
+
+    WA.room.area.onEnter('inside_doorstep').subscribe(() => {
+        console.log('inside');
+        openCloseMessage = WA.ui.displayActionMessage({
+            message: "Press 'space' to open/close the door",
+            callback: () => {
+                WA.state.doorState = !WA.state.doorState;
+            }
+        });
+    });
+
+    // When someone leaves the doorstep (inside the room), we remove the message
+    WA.room.area.onLeave('inside_doorstep').subscribe(() => {
+        if (openCloseMessage !== undefined) {
+            openCloseMessage.remove();
+        }
+    });
 
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes();
         currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
     })
-    
+
     let noteWebsite: any;
 
     WA.room.area.onEnter('bowling').subscribe(async () => {
